1ced4ac934c03428d9aa5c167fe6b293dfce950d
phanikumar4b5 <phanikumar4b5@gmail.com>
2021-07-16 00:50:10 +0530

    changes

--- src/main/java/org/base/Base.java
+++ src/main/java/org/base/Base.java
@@ -9,11 +9,9 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.testng.ITestResult;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
